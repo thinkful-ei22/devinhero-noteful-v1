fbd71254747331fcdb7adfdb44a6244ac21e57a3
dghero <devingreyhero@gmail.com>
2018-06-27 13:18:56 -0500

    Add notes router

---
+++ router/notes.router.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+// Load array of notes
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+router.get('/', (req, res) => {
+  res.json(data.get());
+});
+
+// Endpoints
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+router.get('/notes/:id', (req, res, next) => {
+  const {id} = req.params;
+
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    if(item){
+      res.json(item); //return found item
+    }else{
+      res.send('Item not found');
+    }
+  });
+});
+
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  console.log(req.body);
+  console.log(updateObj);
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
+// 404
+router.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+//Errors
+router.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+
+//Export router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
+// // Load array of notes
+// const data = require('./db/notes');
+// const simDB = require('./db/simDB');
+// const notes = simDB.initialize(data);
 
 // INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 const app = express();
+
+// Setup Router
+const notesRouter = require('./router/notes.router');
 
 //Other setup
 const { PORT } = require('./config');
@@ -20,81 +23,16 @@ app.use(morgan('dev'));
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
-// Parse request body
-app.use(express.json());
-
-
-// Endpoints
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  const {id} = req.params;
-
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    if(item){
-      res.json(item); //return found item
-    }else{
-      res.send('Item not found');
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  console.log(req.body);
-  console.log(updateObj);
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// 404
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
-});
+app.use('/api', notesRouter);
 
-//Errors
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
+// // Parse request body
+// app.use(express.json());
+
 
 // Listener
 app.listen(PORT, function () {
